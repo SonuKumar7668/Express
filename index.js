993e8ec76abefc323930c833b975410871e5868a
Sonu <sonu108rp@gmail.com>
2024-11-26 22:19:42 +0530

    start of ejs

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const express= require("express");
-const app=express()
-// console.dir(app);
-let port=3000;
+const express=require('express');
+const app=express();
+
+let port=3000
+
 app.listen(port,()=>{
-    console.log(`app port is listening at ${port}`)
+    console.log("app is listening ");
+})
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to the server ");
 });
+
+app.get("/:username/:id",(req,res)=>{
+    let {username,id}=req.params;
+    let htmlstr=`<h1>Welcome to the page of @${username}</h1>`
+    // res.send(`Welcome to the page of @${username}`);
+    res.send(htmlstr);
+});
+
+app.get("/search",(req,res)=>{
+    let {q}=req.query;
+    if(!q){
+        res.send("Nothing Searched");
+    }
+    console.log(req.query);
+    res.send(`search result ${q}`);
+})
